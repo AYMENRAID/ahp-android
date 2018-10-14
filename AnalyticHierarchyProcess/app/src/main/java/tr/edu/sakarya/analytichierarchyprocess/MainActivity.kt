bfa865ee3b9119578ddc6c1824de4e6b53edfdd0
emirhanaydin <emirhanaydin95@gmail.com>
2018-10-14 19:30:26 +0300

    Create activity transition to activity_info from activity_main

--- AnalyticHierarchyProcess/app/src/main/java/tr/edu/sakarya/analytichierarchyprocess/MainActivity.kt
+++ AnalyticHierarchyProcess/app/src/main/java/tr/edu/sakarya/analytichierarchyprocess/MainActivity.kt
@@ -1,12 +1,20 @@
 package tr.edu.sakarya.analytichierarchyprocess
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonHelp = findViewById<Button>(R.id.buttonHelp)
+        buttonHelp.setOnClickListener {
+            val intent = Intent(this, InfoActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
