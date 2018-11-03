a7f32a84cc53417742f14226dba4df537d057711
emirhanaydin <emirhanaydin95@gmail.com>
2018-11-03 17:55:53 +0300

    Convert InputFilterMinMax to Kotlin

---
+++ AnalyticHierarchyProcess/app/src/main/java/tr/edu/sakarya/analytichierarchyprocess/InputFilterMinMax.kt
@@ -0,0 +1,19 @@
+package tr.edu.sakarya.analytichierarchyprocess
+
+import android.text.InputFilter
+import android.text.Spanned
+
+class InputFilterMinMax(private val min: Int, private val max: Int) : InputFilter {
+
+    override fun filter(
+        source: CharSequence,
+        start: Int,
+        end: Int,
+        dest: Spanned,
+        dstart: Int,
+        dend: Int
+    ): CharSequence? {
+        val input = Integer.parseInt(dest.toString() + source.toString())
+        return if (input in min..max) null else ""
+    }
+}
