62cd03f473541a9b5744c89861ef93cd68a97f18
emirhanaydin <emirhanaydin95@gmail.com>
2018-11-06 14:45:21 +0300

    Create adding expandable view group child logic with the add button

--- AnalyticHierarchyProcess/app/src/main/java/tr/edu/sakarya/analytichierarchyprocess/CriteriaActivity.kt
+++ AnalyticHierarchyProcess/app/src/main/java/tr/edu/sakarya/analytichierarchyprocess/CriteriaActivity.kt
@@ -13,10 +13,14 @@ import kotlinx.android.synthetic.main.activity_criteria.*
 class CriteriaActivity : AppCompatActivity() {
     private lateinit var criteria: MutableList<Criterion>
     private lateinit var criteriaChildren: MutableMap<Criterion, MutableList<Criterion>>
-    private var expandedGroup = -1
+    private lateinit var criteriaAdapter: CriteriaAdapter
+    private lateinit var listViewCriteria: ExpandableListView
+    private var expandedCriteriaGroup = -1
 
     companion object {
         const val CRITERIA_NAME_MAX_LENGTH = 20
+        const val CRITERIA_VALUE_MIN = 1
+        const val CRITERIA_VALUE_MAX = 9
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,43 +29,92 @@ class CriteriaActivity : AppCompatActivity() {
 
         criteria = mutableListOf()
         criteriaChildren = mutableMapOf()
-        val criteriaAdapter = CriteriaAdapter(this, criteria, criteriaChildren)
-        val listViewCriteria: ExpandableListView = findViewById(R.id.expandable_list_view_criteria)
+        criteriaAdapter = CriteriaAdapter(this, criteria, criteriaChildren)
+        listViewCriteria = findViewById(R.id.expandable_list_view_criteria)
         listViewCriteria.setAdapter(criteriaAdapter)
 
         buttonAddCriteria.setOnClickListener {
-            val linearLayout = LinearLayout(this)
-            linearLayout.orientation = LinearLayout.VERTICAL
-
-            val inputCriteria = EditText(this)
-            inputCriteria.inputType = InputType.TYPE_CLASS_TEXT
-            inputCriteria.filters = arrayOf(InputFilter.LengthFilter(CRITERIA_NAME_MAX_LENGTH))
-            inputCriteria.hint = getString(R.string.criterion)
-
-            linearLayout.addView(inputCriteria)
-
-            val builder: AlertDialog.Builder = AlertDialog.Builder(this)
-            builder.setTitle(getString(R.string.add_criterion))
-            builder.setView(linearLayout)
-
-            builder.setPositiveButton(getString(R.string.add)) { _, _ ->
-                val criteriaInput = inputCriteria.text.toString()
-                val criterion = Criterion(criteriaInput)
-                criteria.add(criterion)
-                criteriaChildren[criterion] = mutableListOf()
-
-                criteriaAdapter.notifyDataSetChanged()
-            }
-            builder.setNegativeButton(getString(R.string.cancel)) { dialog, _ -> dialog.cancel() }
-
-            val alertDialog: AlertDialog = builder.create()
-            alertDialog.show()
+            if (expandedCriteriaGroup < 0)
+                onClickAddNewCriteriaGroup()
+            else
+                onClickAddNewCriterionChild()
         }
 
         listViewCriteria.setOnGroupExpandListener { groupPosition ->
-            if (expandedGroup == groupPosition) return@setOnGroupExpandListener
-            listViewCriteria.collapseGroup(expandedGroup)
-            expandedGroup = groupPosition
+            val previousPosition = expandedCriteriaGroup
+            expandedCriteriaGroup = groupPosition
+
+            if (previousPosition < 0) return@setOnGroupExpandListener
+
+            listViewCriteria.collapseGroup(previousPosition)
         }
+
+        listViewCriteria.setOnGroupCollapseListener { groupPosition ->
+            if (expandedCriteriaGroup == groupPosition)
+                expandedCriteriaGroup = -1
+        }
+    }
+
+    private fun onClickAddNewCriteriaGroup() {
+        val inputCriteria = EditText(this)
+        inputCriteria.inputType = InputType.TYPE_CLASS_TEXT
+        inputCriteria.filters = arrayOf(InputFilter.LengthFilter(CRITERIA_NAME_MAX_LENGTH))
+        inputCriteria.hint = getString(R.string.criterion)
+
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle(getString(R.string.add_criterion))
+        builder.setView(inputCriteria)
+
+        builder.setPositiveButton(getString(R.string.add)) { _, _ ->
+            val input = inputCriteria.text.toString()
+            val criterion = Criterion(input)
+            criteria.add(criterion)
+            criteriaChildren[criterion] = mutableListOf()
+
+            criteriaAdapter.notifyDataSetChanged()
+        }
+        builder.setNegativeButton(getString(R.string.cancel)) { dialog, _ -> dialog.cancel() }
+
+        val alertDialog: AlertDialog = builder.create()
+        alertDialog.show()
+    }
+
+    private fun onClickAddNewCriterionChild() {
+        val inputCriteria = EditText(this)
+        inputCriteria.inputType = InputType.TYPE_CLASS_TEXT
+        inputCriteria.filters = arrayOf(InputFilter.LengthFilter(CRITERIA_NAME_MAX_LENGTH))
+        inputCriteria.hint = getString(R.string.criterion)
+
+        val inputValue = EditText(this)
+        inputValue.inputType = InputType.TYPE_CLASS_NUMBER
+        inputValue.filters = arrayOf(InputFilterMinMax(CRITERIA_VALUE_MIN, CRITERIA_VALUE_MAX))
+        inputValue.hint = getString(R.string.value)
+
+        val linearLayout = LinearLayout(this)
+        linearLayout.orientation = LinearLayout.VERTICAL
+        linearLayout.addView(inputCriteria)
+        linearLayout.addView(inputValue)
+
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle(getString(R.string.add_criterion))
+        builder.setView(linearLayout)
+
+        builder.setPositiveButton(getString(R.string.add)) { _, _ ->
+            val inputCriteriaText = inputCriteria.text.toString()
+
+            val inputValueText = inputValue.text.toString()
+            if (inputValueText == "") return@setPositiveButton
+            val inputValueInt = inputValueText.toInt()
+
+            val criterion = Criterion(inputCriteriaText, inputValueInt)
+            val group = criteria[expandedCriteriaGroup]
+            criteriaChildren[group]?.add(criterion) ?: return@setPositiveButton
+
+            criteriaAdapter.notifyDataSetChanged()
+        }
+        builder.setNegativeButton(getString(R.string.cancel)) { dialog, _ -> dialog.cancel() }
+
+        val alertDialog: AlertDialog = builder.create()
+        alertDialog.show()
     }
 }
